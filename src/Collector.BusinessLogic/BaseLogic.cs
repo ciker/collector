76670d1a7a84e7364ee2466068be602f84426af8
binlyzhuo <binlyzhuo@outlook.com>
2015-06-12 16:08:18 +0800

    ~commit

---
+++ src/Collector.BusinessLogic/BaseLogic.cs
@@ -0,0 +1,53 @@
+﻿using Collector.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collector.BusinessLogic
+{
+    public class BaseLogic<T>
+        where T:class
+    {
+        protected readonly BaseData<T> data;
+        public BaseLogic(BaseData<T> baseData)
+        {
+            data = baseData;
+        }
+
+        
+
+        public bool IsNew(T item)
+        {
+            return data.IsNew(item);
+        }
+
+        public void Save(T item)
+        {
+            data.Save(item);
+        }
+
+        public int Insert(T item)
+        {
+            return Convert.ToInt32(data.Insert(item));
+        }
+
+        public int Update(T item)
+        {
+           return data.Update(item);
+        }
+
+        public T Get(int id)
+        {
+            return data.Single(id);
+        }
+
+        public bool Delete(int id)
+        {
+           return data.Delete(id)>0;
+        }
+
+
+    }
+}
---
+++ src/Collector.BusinessLogic/UserBusinessLogic.cs
@@ -0,0 +1,22 @@
+﻿using Collector.Data;
+using Collector.Model.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collector.BusinessLogic
+{
+    public class UserBusinessLogic:BaseLogic<T_User>
+    {
+        private readonly UserData userData;
+        public UserBusinessLogic(UserData userData) :
+            base(userData)
+        {
+            userData = ((UserData)this.data);
+        }
+
+        
+    }
+}
